c2d32521b362adf5bb055475a2d06055a67eae82
gamsistemasinteligentes-png <gamsistemasinteligentes@gmail.com>
2026-03-05 16:34:21 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'rancho-pwa-v4';
+const assets = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+  )));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
